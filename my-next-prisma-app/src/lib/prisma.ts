2dccc6f1ace10f02c8982969cfd2f52ced90a07c
joespags9 <joespags9@gmail.com>
2025-11-19 18:16:05 -0500

    Schema Fixes

--- my-next-prisma-app/src/lib/prisma.ts
+++ my-next-prisma-app/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 declare global {
   // Add a global variable for dev to prevent multiple PrismaClient instances
